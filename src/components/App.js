737ebe4d0818eb806e20d8d012dd2450886e335c
Pius <emefiene.pius@yahoo.com>
2022-09-27 00:13:06 -0500

    updated App

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,41 @@ import PhysicianContainer from "./PhysicianContainer";
 import PatientContainer from "./PatientContainer";
 import PatientDetails from "./PatientDetails";
 
+
+
 function App() {
+
+  const [physiciansData, setPhysiciansData] = useState([])
+  const [patientsData, setPatientsData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/physicians")
+    .then(res => res.json())
+    .then(data => {
+      setPhysiciansData(data)
+    })
+
+    fetch("http://localhost:9292/patients")
+    .then(res => res.json())
+    .then(data => {
+      setPatientsData(data)
+    })
+
+  },[])
   return (
     <div>
-      
+      <Home/>
+      <Switch>
+       <Route exact path="/physicians">
+          <PhysicianContainer physiciansData={physiciansData}/>
+        </Route>
+        <Route exact path="/patients">
+          <PatientContainer patientsData={patientsData}/>
+        </Route>
+        <Route exact path="/patients/:id">
+          <PatientDetails/>
+        </Route>
+      </Switch>
     </div>
   );
 }
