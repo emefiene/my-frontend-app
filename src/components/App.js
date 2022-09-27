250a2bd7c060c8f6435548fe80e4245b88f18fd8
Pius <emefiene.pius@yahoo.com>
2022-09-27 00:10:29 -0500

    updated App

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import { Switch, useHistory, Route} from "react-router-dom";
+import Home from "./Home";
+import PhysicianContainer from "./PhysicianContainer";
+import PatientContainer from "./PatientContainer";
+import PatientDetails from "./PatientDetails";
 
 function App() {
   return (
