54c2d712426691c79f91b0c0883f4b3d37ef05d4
Pius <emefiene.pius@yahoo.com>
2022-10-01 02:39:42 -0500

    PatientReview complete

--- src/components/PatientReviews.js
+++ src/components/PatientReviews.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-const PatientReviews = ({reviewObj:{comments,time}}) => {
-    
+const PatientReviews = ({reviewObj:{comments,time,patient_id}}) => {
+
+ const [patientsData, setPatientsData] = useState([])
+
+ useEffect(() => {
+   fetch("http://localhost:9292/patients")
+    .then(res => res.json())
+    .then(data => {
+      setPatientsData(data)
+    })
+
+ }, [])
+ 
+ const p = patientsData.map(patientName => {
+    if(patient_id === patientName.id){
+      return (patientName.first_name)
+    }
+   })
+
   return(
     <div>
+       <h2>Name: {p}</h2>
        <strong>Comment :</strong><h4>{comments}</h4>
        <p>Time: {time}</p>
     </div>
