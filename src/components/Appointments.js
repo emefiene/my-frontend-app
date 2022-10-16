035ad2adc9deb786d41bd22b087fdd8d5f6990e8
Pius <emefiene.pius@yahoo.com>
2022-10-16 03:56:54 -0500

    updated appointment

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -1,51 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import styled from "styled-components"
 
 
-const Appointments = ({appointmentObj:{patient_id,physician_id, date}}) => {
- const [physicianData, setPhysicianData] = useState([])
- const [patientsData, setPatientsData] = useState([])
+const Appointments = ({appointmentObj:{date, patient,physician}}) => {
 
- useEffect(() => {
 
-  fetch("http://localhost:9292/physicians")
-    .then(res => res.json())
-    .then(data => {
-      setPhysicianData(data)
-    })
-
-    fetch("http://localhost:9292/patients")
-    .then(res => res.json())
-    .then(data => {
-      setPatientsData(data)
-    })
-
- }, [])
-
- const p = patientsData.map(patientName => {
-  if(patient_id === patientName.id){
-    return (patientName.name)
-  }
- })
-
- const y = physicianData.map(physicianName => {
-  if(physician_id === physicianName.id){
-    return (physicianName.specialty)
-  }
- })
-
- const x = physicianData.map(physicianName => {
-   if(physician_id === physicianName.id){
-     return (physicianName.name)
-   }
- })
   
   return (
     <Card>
-     <h2 style={{color: "blue"}}>Dr :{x}</h2><h4>Specialty :{y}</h4>
-
-      <h4 style={{color: "red"}}>Patient Name: {p}</h4>
-      <h3>{date}</h3>
+      <h2>Name: {patient.name}</h2>
+      <h4>Address:  {patient.address}</h4>
+      <h2>Name:  Dr. {physician.name}</h2>
+      <h4>Specialty:  {physician.specialty}</h4>
+      <h3>Date: {date}</h3>
     </Card>
   )
 }
